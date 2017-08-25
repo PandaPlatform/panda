f00dabf72da1935c70b5897c2229a1d03c84405d
Ioannis Papikas <papikas.ioan@gmail.com>
2017-08-25 11:55:26 +0300

    Improve route loading logic

---
+++ resources/routes/pages.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the Panda framework.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use App\Controllers\Pages\IndexController;
+use Panda\Support\Facades\Route;
+
+Route::get('/', IndexController::class . '@index');
