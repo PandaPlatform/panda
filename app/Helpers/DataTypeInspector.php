ea1a8a4abbc32bce6e58f4ee35db48e9203528f3
Ioannis Papikas <papikas.ioan@gmail.com>
2017-08-25 11:54:59 +0300

    Add Helpers

---
+++ app/Helpers/DataTypeInspector.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Helpers;
+
+use ReflectionClass;
+use ReflectionProperty;
+
+/**
+ * Class DataTypeInspector
+ * @package App\Helpers
+ */
+class DataTypeInspector
+{
+    /**
+     * @param mixed $object
+     *
+     * @return array|null
+     */
+    public static function getObjectMethodsNames($object)
+    {
+        // Check if object is class
+        $class = get_class($object);
+        if (!$class) {
+            return null;
+        }
+
+        return get_class_methods($class);
+    }
+
+    /**
+     * @param mixed $object
+     *
+     * @return array
+     */
+    public static function getObjectConstants($object)
+    {
+        // Normalize class
+        $class = is_string($object) ? $object : get_class($object);
+
+        // Create Reflection
+        $reflection = new ReflectionClass($class);
+
+        return $reflection->getConstants();
+    }
+
+    /**
+     * @param mixed $object
+     *
+     * @return null|ReflectionProperty[]
+     */
+    public static function getObjectProperties($object)
+    {
+        // Check if object is valid
+        if (!$object) {
+            return null;
+        }
+
+        // Create Reflection
+        $reflection = new ReflectionClass($object);
+
+        return $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+    }
+
+    /**
+     * @param mixed  $class
+     * @param string $interface
+     *
+     * @return bool
+     */
+    public static function implementsInterface($class, $interface)
+    {
+        $interfaces = class_implements($class);
+        foreach ($interfaces as $if) {
+            if ($if == $interface) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ app/Helpers/HttpHelper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Helpers;
+
+use Panda\Support\Helpers\ArrayHelper;
+
+/**
+ * Class HttpHelper
+ * @package App\Helpers
+ */
+class HttpHelper
+{
+    const METHOD_POST = 'post';
+    const METHOD_GET = 'get';
+    const METHOD_PUT = 'put';
+    const METHOD_PATCH = 'patch';
+    const METHOD_DELETE = 'delete';
+    const METHOD_DELETE_WITH_CONTENT = 'delete_with_content';
+    const METHOD_ALIAS_CREATE = 'create';
+    const METHOD_ALIAS_UPDATE_WITH_CONTENT = 'update_with_content';
+    const METHOD_ALIAS_UPDATE_WITHOUT_CONTENT = 'update_without_content';
+    const METHOD_ALIAS_SUCCESS = "success";
+
+    /**
+     * @var array
+     */
+    protected static $successStatusCodes = [
+        self::METHOD_POST => 201,
+        self::METHOD_GET => 200,
+        self::METHOD_PUT => 200,
+        self::METHOD_PATCH => 200,
+        self::METHOD_DELETE => 204,
+        self::METHOD_DELETE_WITH_CONTENT => 200,
+        self::METHOD_ALIAS_CREATE => 201,
+        self::METHOD_ALIAS_UPDATE_WITH_CONTENT => 200,
+        self::METHOD_ALIAS_UPDATE_WITHOUT_CONTENT => 204,
+        self::METHOD_ALIAS_SUCCESS => 200,
+    ];
+
+    /**
+     * @param string $method
+     *
+     * @return int
+     */
+    public static function getStatusCodeOnSuccess($method)
+    {
+        return ArrayHelper::get(self::$successStatusCodes, strtolower($method), 200);
+    }
+}
