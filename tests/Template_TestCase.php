f0fff6d2854da2089b80abff0e880c47f819a8dc
Ioannis Papikas <papikas.ioan@gmail.com>
2017-08-25 11:54:16 +0300

    Add more base test cases

---
+++ tests/Template_TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Panda framework.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests;
+
+/**
+ * Class Template_TestCase
+ * @package Tests
+ */
+class Template_TestCase extends Base_TestCase
+{
+    /**
+     * @throws \PHPUnit\Framework\AssertionFailedError
+     */
+    public function testCase()
+    {
+        $this->assertTrue(true);
+    }
+}
